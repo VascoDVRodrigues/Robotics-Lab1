5fe3babd505ad99e50838be3391dc390237455e9
VascoDVRodrigues <vascodvrodrigues@gmail.com>
2021-12-09 10:51:50 +0000

    added simple code to calculate some T matrices

---
+++ matrix.py
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+from niryo_one_python.api.niryo_one_api import *
+import rospy
+import time
+import math
+
+n = NiryoOne()
+n.calibrate()
+
+joints = n.get_joints()
+
+class Robot:
+    def __init__(self, self.joints):
+        self.name = name
+        self.theta = self.joints
+        self.dab = 103
+        self.dbc = 80
+        self.dcd = 210
+    
+    def calculate_transformation_matrices(self):
+        Tba = [[math.cos(theta[0]),math.sin(theta[0],0,0],
+            [-1*math.sin(theta[0]),math.cos(theta[0]),0,0],
+            [0,0,1,self.dab],
+            [0,0,0,1]]
+
+        Tbc = [[1,0,0,0],
+            [0, math.cos(theta[1]), -1 * math.sin(theta[1], self.dbc * math.cos(theta[1]))],
+            [0,math.sin(theta[1]), math.cos(theta[1], self.dbc * math.sin(theta[1])],
+            [0,0,0,1]]
+
+
+
+robot = Robot("alberto",joints)
+robot.calculate_transformation_matrix()
+
